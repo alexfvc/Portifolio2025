0c7c0e5990c5ac945de5c733a5e9f162ad8e68b7
alexcosta <alexfvc@gmail.com>
2025-07-30 12:57:48 -0300

    melhoria no vizual cards

--- src/components/table-grid/tableGrid.tsx
+++ src/components/table-grid/tableGrid.tsx
@@ -6,8 +6,8 @@ const HomeTableGridComponent = ({children}:ITableGrid) => {
 
 
     return(
-        <Stack display={'flex'} maxWidth={{sm:600,md:'100%'}} justifyContent={'center'} alignItems={'center'} spacing={2}   py={2}>
-            <Grid container justifyContent={'center'} spacing={{xs:5, md:5}}   p={3} >
+        <Stack display={'flex'} maxWidth={{sm:550,md:'100%'}} justifyContent={'center'} alignItems={'center'} spacing={2} pb={2}>
+            <Grid container justifyContent={'center'} spacing={{sm:2, md:5}} pb={1} >
                     {React.Children.map(children, (child, index) => (
                         <Grid display={'flex'}  key={index}>
                             {child}
