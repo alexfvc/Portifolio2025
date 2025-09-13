a2f3129c6969340dd605b1d82b29e5393bc7642d
alexcosta <alexfvc@gmail.com>
2025-09-13 17:37:21 -0300

    fix route

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-
-import { Route, Routes, BrowserRouter, Navigate } from 'react-router-dom'
+import { Routes, Route, Navigate } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home'
 import CalendarioDocente from './pages/calendarioDocente'
+
 function App() {
-
-
   return (
-    <BrowserRouter basename="/Portifolio2025">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calendario-docente" element={<CalendarioDocente />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </BrowserRouter>
-);
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calendario-docente" element={<CalendarioDocente />} />
+      <Route path="*" element={<Navigate to="/" replace />} />
+    </Routes>
+  )
 }
-export default App;
+
+export default App
