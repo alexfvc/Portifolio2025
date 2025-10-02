b6fdd735b121184e6342d047e7f0e91a301feeb7
Alex Costa <alex.costa@cenix.com.br>
2025-10-02 15:51:08 -0300

    rota correta /Portfolio2025

--- src/main.tsx
+++ src/main.tsx
@@ -7,7 +7,7 @@ import './index.css';
 const theme = createTheme();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-<BrowserRouter basename="/Portifolio2025">
+<BrowserRouter basename="/Portifolio2025/">
   <ThemeProvider theme={theme}>
     <App />
   </ThemeProvider>
